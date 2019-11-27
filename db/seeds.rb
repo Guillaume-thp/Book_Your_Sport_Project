8ed99b530f074a7cd012aa49a294695dd35449db
Guillaume Vidale <guillaume.vidale@gmail.com>
2019-11-27 18:26:24 +0100

    commit du front de la view index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+require 'table_print'
+
+
+User.destroy_all
+p  "All user has destroy"
+
+10.times do |x|
+	User.create!(
+		email: "#{x}@yopmail.com",
+        password: "azerty")
+    end
