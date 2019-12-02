dc60d5f98b11b14f2b6c3e90e89e9585824f9723
MichaelKTHP <michaelkrasnovsky@gmail.com>
2019-12-02 15:20:43 +0100

    dossier service au bon endroit

---
+++ app/services/scrap.rb
@@ -0,0 +1,46 @@
+class Scrap
+  def initialize
+  end
+
+
+  def perform
+    browser = Watir::Browser.new(:firefox)
+browser.goto 'https://my.urbansoccer.fr/user?goto=reserver'
+email_field = browser.text_field(type: 'email')
+password_field = browser.text_field(type: 'password')
+email_field.set("michaelyohang@gmail.com")
+password_field.set("MKthp10bys")
+password_field.send_keys(:enter)
+
+idf = browser.path(id: "IDF")
+idf.double_click
+
+puteaux = browser.div(text: "Puteaux")
+puteaux.double_click
+# cliquer sur la fleche droite du calendrier pour le mois suivant
+ #browser.span(class:["DayPicker-NavButton", "DayPicker-NavButton--next"]).click
+
+
+date = browser.div(text: "21") # Choisir le jour, A changer par la variable tiré du formulaire de recherche
+date.click
+
+
+#dropdown heure
+browser.element(:xpath => "/html/body/div/div/div/div[4]/div/div/div[2]/div[2]/div[2]/div[2]/div[1]/div/select[1]/option[@value='15h00']").click
+#dropdown durée
+browser.element(:xpath => "/html/body/div/div/div/div[4]/div/div/div[2]/div[2]/div[2]/div[2]/div[1]/div/select[2]/option[@value='120']").click
+
+#Type de terrain. Remplacer un tiret par un underscore dans un attribut.
+browser.input(data_label:"3 contre 3").click
+
+return browser.div(class: "reservation-header").text
+  end
+end
+
+
+#type de terrain : browser.element(:xpath => "//div[2]/span[2]").text
+# date : browser.element(:xpath => "//div[3]/span[2]").text
+# durée : browser.element(:xpath => "//div[2]/div[2]/span[2]").text
+# browser.element(:xpath => "//div[2]/div[3]/span[2]).text
+# prix : browser.element(:xpath => "//div[2]/div[3]/span[2]").text
+
